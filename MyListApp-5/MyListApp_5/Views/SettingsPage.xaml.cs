fd1e791bbc127c2be5636e6d726a62689a562b54
Tim Hastings <thastings@objectconsulting.com.au>
2018-02-18 12:17:55 +1100

    Add project files.

---
+++ MyListApp-5/MyListApp_5/Views/SettingsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyListApp_5
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+            //BindingContext = new ContentPageViewModel();
+        }
+
+        private void myButton_Clicked(object sender, EventArgs e)
+        {
+            myLabel.Text = myEntry.Text;
+        }
+    }
+
+    
+    
+}
